c8405bea45d5c09a5296a4749333f4870a92b3f3
gaasefod <gaasefod@tutanota.com>
2023-01-26 19:13:24 +0100

    Test: Add jest test

---
+++ __tests__/jest.test.ts
@@ -0,0 +1,43 @@
+import * as Math from "@/utils/math";
+
+describe("Math", () => {
+  test("2 + 2 equals 4", () => {
+    expect(Math.sum(2, 2)).toBe(4);
+  });
+
+  test("2 - 2 equals 0", () => {
+    expect(Math.subtract(2, 2)).toBe(0);
+  });
+
+  test("2 / 2 equals 1", () => {
+    expect(Math.divide(2, 2)).toBe(1);
+  });
+
+  test("2 * 2 equals 4", () => {
+    expect(Math.multiply(2, 2)).toBe(4);
+  });
+
+  test("2 % 2 equals 0", () => {
+    expect(Math.modulo(2, 2)).toBe(0);
+  });
+
+  test("3 + -1 equals 2", () => {
+    expect(Math.sum(3, -1)).toBe(2);
+  });
+
+  test("3 - -1 equals 4", () => {
+    expect(Math.subtract(3, -1)).toBe(4);
+  });
+
+  test("3 / -1 equals -3", () => {
+    expect(Math.divide(3, -1)).toBe(-3);
+  });
+
+  test("3 * -1 equals -3", () => {
+    expect(Math.multiply(3, -1)).toBe(-3);
+  });
+
+  test("3 % -1 equals 0", () => {
+    expect(Math.modulo(3, -1)).toBe(0);
+  });
+});
---
+++ utils/math.ts
@@ -0,0 +1,11 @@
+const sum = (int1: number, int2: number) => int1 + int2;
+
+const subtract = (int1: number, int2: number) => int1 - int2;
+
+const divide = (int1: number, int2: number) => int1 / int2;
+
+const multiply = (int1: number, int2: number) => int1 * int2;
+
+const modulo = (int1: number, int2: number) => int1 % int2;
+
+export { sum, subtract, divide, multiply, modulo };
